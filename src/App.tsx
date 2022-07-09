04156f454da2d6935df5ebb0434e0e657e4763df
kitou88C39 <kanfutrooper@icloud.com>
2022-07-09 14:40:56 +0900

    forth commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Movie from './components/Movie';
 import NavBar from './components/NavBar';
 import axios from 'axios';
 import loading_spinner from './assets/loading_spinner.gif';
-import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 async function getMovies(pageNo: number) {
   const res = await axios.get(
@@ -27,6 +27,11 @@ function App() {
       });
   }, [pageNo]);
 
+  const handlePageClick = (pageNo) => {
+    const pageNo = pageNo['selected']; // クリックした部分のページ数が{selected: 2}のような形で返ってくる
+    setMovies(pageNo); // offsetを変更し、表示開始するアイテムの番号を変更
+  };
+
   if (movies === 'Loading' || !movies || movies.length === 0)
     return (
       <div className='flex items-center justify-center h-screen bg-gray-200'>
@@ -37,7 +42,22 @@ function App() {
     return (
       <div className='flex flex-col h-full max-h-screen bg-black '>
         <NavBar />
-        <div className='w-[250] mt-5 pb-10 font-bold pt-8'>
+        <div className='box-border bg-black text-white font-sans .m-0 grid grid-cols-5 gap-5 items-center'>
+          {movies.length > 0 &&
+            Array.isArray(movies) &&
+            movies.length > 0 &&
+            movies.map((movie: any) => <Movie key={movie.id} {...movie} />)}
+        </div>
+        <ReactPaginate
+          previousLabel={'← Previous'}
+          nextLabel={'Next →'}
+          breakLabel={'...'}
+          pageCount={1000}
+          marginPagesDisplayed={3}
+          pageRangeDisplayed={3}
+          onPageChange={handlePageClick}
+        />
+        {/* <div className='w-[250] mt-5 pb-10 font-bold pt-8'>
           <button
             className='px-4 mr-2 bg-white border-2 rounded-full hover:border-black hover:font-bold'
             onClick={() => {
@@ -47,17 +67,7 @@ function App() {
           >
             Previous
           </button>
-
-          <ul>
-            {movies.length > 0 &&
-              Array.isArray(movies) &&
-              movies.length > 0 &&
-              movies.map((movie) => (
-                <li key={movie.id}>
-                  <Link to={movie.id}>{pageNo}</Link>
-                </li>
-              ))}
-          </ul>
+          {pageNo}
 
           <button
             className='px-4 ml-2 bg-white border-2 rounded-full hover:border-black hover:font-bold'
@@ -68,13 +78,7 @@ function App() {
           >
             Next
           </button>
-        </div>
-        <div className='box-border bg-black text-white font-sans .m-0 grid grid-cols-5 gap-5 items-center'>
-          {movies.length > 0 &&
-            Array.isArray(movies) &&
-            movies.length > 0 &&
-            movies.map((movie: any) => <Movie key={movie.id} {...movie} />)}
-        </div>
+        </div>*/}
       </div>
     );
 }
